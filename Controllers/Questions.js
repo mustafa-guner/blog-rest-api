22bc84c7c463f11d02f96cc715a4a330a8b3ce76
slex1one-Musdy <legend123412@outlook.com>
2021-05-11 13:33:15 +0300

    Ask Question route completed

--- Controllers/Questions.js
+++ Controllers/Questions.js
@@ -1,6 +1,32 @@
-
+const asyncHandler = require("express-async-handler");
+const Question = require("../Models/Question");
+const CustomError = require("../Helpers/Custom Error/CustomError");
 
 module.exports = {
+
+    createQuestion: asyncHandler( async (req,res,next)=>{
+        
+        //We have to create a question from the req.body ({title,content e.g})
+        const informations = req.body;
+
+        //Create question
+        const question = await new Question({
+            ...informations,
+            //We have to know which user is going to post this question (take the user id from request)
+            user:req.user.id
+        })
+
+        //SAVE QUESTION
+        await question.save();
+
+        req.status(200).json({
+            success:true,
+            message:"Question is created succesfuly",
+            data:question
+        })
+
+    }),
+
     getAllQuestions: (req,res,next)=>{
        res.status(200).json({
            message:"Questions are rendering",
--- Routes/Questions.js
+++ Routes/Questions.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const QuestionRoute = express.Router();
-const {getAllQuestions} = require("../Controllers/Questions");
+const {getAccessToRoute} = require("../Middlewares/Authorization/auth")
+const {getAllQuestions,createQuestion} = require("../Controllers/Questions");
 
+QuestionRoute.post("/ask",getAccessToRoute,createQuestion); // In orde to ask the question you have to be logged in (TOKEN)
 QuestionRoute.get("/",getAllQuestions);
 
 module.exports = QuestionRoute;
